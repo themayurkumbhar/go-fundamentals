083b6260c55daf1f68560921445fe3b0ff93ec7a
Mayur Kumbhar <mayur.kumbhar@outlook.com>
2022-01-23 18:59:16 +0530

    update with conditional if-else

--- conditionals/if-example.go
+++ conditionals/if-example.go
@@ -3,11 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	totalWatchedMins := 100
+	totalWatchedMins := 600
 	totalConentMins := 600
 
 	if totalConentMins > totalWatchedMins {
 		fmt.Println("You have to watch more mins of course!")
+	} else if totalWatchedMins == totalConentMins {
+		fmt.Println("You have completed the course!")
+	} else {
+		fmt.Println("Total watch content must not be greater than total content")
 	}
 
 }
