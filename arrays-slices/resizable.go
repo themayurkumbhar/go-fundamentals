88918433e36f10eb429f9ea4f6a7b39f17e8e6a6
Mayur Kumbhar <mayur.kumbhar@outlook.com>
2022-01-26 22:59:24 +0530

    udpate resiable with append new slice
    Issue-id: ARRAYS-06

--- arrays-slices/resizable.go
+++ arrays-slices/resizable.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	baseSlice := make([]int, 2, 4)
@@ -8,9 +10,15 @@ func main() {
 	fmt.Println("will start adding elements and see how the size and capacity will grow")
 
 	for i := 0; i < 10; i++ {
-		baseSlice = append(baseSlice, i+1)
+		baseSlice = append(baseSlice, 1+i)
 		fmt.Println("baseslice length is:", len(baseSlice), "with capacity", cap(baseSlice))
 	}
 
 	//output will show you capacity is increase by 2x!
+
+	//append whole slice
+	fmt.Println(baseSlice)
+	newSlice := []int{10, 20, 30}
+	newSlice = append(newSlice, baseSlice...) // here ... signifies the all the elements in slice
+	fmt.Println(newSlice)
 }
