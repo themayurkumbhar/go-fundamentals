5aff6f98a5018b42f09306e3a193c892f0de72de
Mayur Kumbhar <mayur.kumbhar@outlook.com>
2022-01-27 21:43:05 +0530

    update maps add, update, delete and access uses
    Issue-id: MAPS-07

--- maps-in-go/map-iteration.go
+++ maps-in-go/map-iteration.go
@@ -16,4 +16,17 @@ func main() {
 	for mapKey, mapValue := range mapInteration {
 		fmt.Println("Key is:", mapKey, "-> value is:", mapValue)
 	}
+
+	//access specific map value
+	fmt.Println(mapInteration[5])
+
+	//update specific map value, //if the key dosent exists it will create map entry
+	fmt.Println("before update:", mapInteration[3])
+	mapInteration[3] = "SomeNewVal"
+	fmt.Println("after update:", mapInteration[3])
+
+	//delete entry from map
+	fmt.Println("before deletion:", mapInteration)
+	delete(mapInteration, 3)
+	fmt.Println("after deletion:", mapInteration)
 }
