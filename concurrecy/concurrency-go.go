27aa66d2f7d5ea92de5882997759bb8af6a349a0
Mayur Kumbhar <mayur.kumbhar@outlook.com>
2022-02-01 18:59:50 +0530

    update concurrency with go routine
    Issue-id: CONCURRENCY-09

--- concurrecy/concurrency-go.go
+++ concurrecy/concurrency-go.go
@@ -2,20 +2,31 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
 
+	// as the main is also go-routine in go, if it complete all the go-routines will complete.
+	// so will this to wait for the these to complete
+	var waitGrp sync.WaitGroup
+	waitGrp.Add(2) //wait for 2 roitines to complete before exit
+
 	// well if you thinking what is code, its actually anonymous functions - a function has no name :P
 	// you can create anonymous function just declare function without name
 	// add () end of the function which executues the anonymous function
-	func() {
+	// adding go infront of function makes it go routine
+	go func() {
+		defer waitGrp.Done()
 		time.Sleep(5 * time.Second)
 		fmt.Println("go-fundamentals")
 	}()
 
-	func() {
-		fmt.Println("leaning-concurrency")
+	go func() {
+		defer waitGrp.Done()
+		fmt.Println("learning-concurrency")
 	}()
+
+	waitGrp.Wait()
 }
